f3e7abebb0d007cc89ce3c0a48a46c45c6bf9f4e
nayeem407 <sabbirahmednayeem941@gmail.com>
2021-09-02 20:34:12 +0600

    last commit

--- js/app.js
+++ js/app.js
@@ -7,18 +7,23 @@ searchButton.addEventListener('click' ,function(){
     const searchText = searchInput.value
     searchInput.value= '';
     if(searchText === ''){
+        // errorHandle.innerText= "Search field cannot be empty"
+
         errorHandle.innerHTML = `
         <h1 class="text-center text-warning d-inline bg-secondary " > Search field cannot be Empty</h1>
         `
+        return
+
     }    
-    else {
     
-        const url = `https://openlibrary.org/search.json?q=${searchText}`
+    else{    
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
 
     fetch (url)
     .then(res => res.json())
     .then(data => displayResult(data.docs))
-        
+    .finally(() => searchInput.value === "")
+    
     }
     
     
@@ -39,12 +44,12 @@ const displayResult = books =>{
         div.classList.add('col')
         div.innerHTML= `
         <div class="card">
-                <img src="${book.cover_i}" class="card-img-top" alt="...">
+                <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                      <h5 class="card-title">${book.title}</h5>
                      <h4>${book.author_name}</h4>
                      <h6>${book.publish_date}</h6>
-                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                     <p class="card-text"></p>
                 </div>
           </div>
         `
